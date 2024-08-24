c6b7c4f3f9339e5eb9c409ed667828dad3626f44
CydKnocking <yingdianc@163.com>
2024-08-24 15:49:18 +0800

    update

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 Notebook: https://note.knocking.moe
 
-This is Knockning's personal notebook.
+This is Knocking's personal notebook.
 
 这里是Knocking的个人笔记本.
 
--- update_recent.py
+++ update_recent.py
@@ -7,7 +7,7 @@ INDEX_HEADER = \
 """
 # Knocking's personal notebook
 
-This is Knockning's personal notebook.
+This is Knocking's personal notebook.
 
 这里是Knocking的个人笔记本.
 
